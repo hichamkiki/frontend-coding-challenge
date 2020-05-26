60270b3fed22945d2c6e64b1f58416fff9ed166c
hichamkiki <m.kikihicham@gmail.com>
2020-05-26 10:32:58 +0100

    feat01: add angular material and create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { ListReposComponent } from './components/list-repos/list-repos.component';
 
+// Components Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListReposComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
